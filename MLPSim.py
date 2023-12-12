2060b2c69f721c8a989093f62f8e3998ad2b1648
naderabdullah <112974974+naderabdullah@users.noreply.github.com>
2023-12-11 19:49:16 -0500

    Update MLPSim.py

--- MLPSim.py
+++ MLPSim.py
@@ -1,105 +1,76 @@
 import numpy as np
 
-def backprop(weights, inputs, targets, ninputs, nhiddenneurons, noutputs):
-    ninputs += 1  # +1 for bias node
-    N = len(weights)
-    
-    wi = weights[:ninputs * nhiddenneurons].reshape((ninputs, nhiddenneurons))
-    wo = weights[ninputs * nhiddenneurons:N].reshape((nhiddenneurons, noutputs))
-    
-    ai = np.append(inputs, 1)  # 1 for bias node
-    
-    netW = np.dot(ai, wi)
-    ah = np.tanh(netW)
-    
-    netW = np.dot(ah, wo)
-    ao = np.tanh(netW)
-    
-    output_errors = (1.0 - ao ** 2) * (targets - ao)
-    error = np.dot(output_errors, wo.T)
-    hidden_errors = (1.0 - ah ** 2) * error
-    
-    change = np.outer(ai, hidden_errors)
-    wi += (1 / ninputs) * change
-    
-    netW = np.dot(ai, wi)
-    ah = np.tanh(netW)
-    
-    change = np.outer(ah, output_errors)
-    wo += (1 / (nhiddenneurons + 1)) * change
-    
-    weights = np.concatenate((wi.flatten(), wo.flatten()))
-    
+def backprop(weights, inputs, targets, layer_sizes, learning_rate=0.01):
+    # Forward Pass
+    activations = [np.append(inputs, 1)]  # Include bias in inputs
+    for i in range(len(layer_sizes) - 1):
+        net_input = np.dot(activations[-1], weights[f'W{i}'])
+        activation = np.tanh(net_input)
+        activation = np.append(activation, 1) if i < len(layer_sizes) - 2 else activation
+        activations.append(activation)
+
+    # Compute Output Error
+    output_errors = (1.0 - activations[-1] ** 2) * (targets - activations[-1])
+
+    # Backward Pass
+    for i in reversed(range(len(layer_sizes) - 1)):
+        if i != len(layer_sizes) - 2:  # Not the output layer
+            # Calculate error for hidden layers
+            error = (1.0 - activations[i + 1][:-1] ** 2) * np.dot(output_errors, weights[f'W{i+1}'].T)[:-1]
+        else:
+            # Error for the output layer
+            error = output_errors
+
+        # Compute Gradient and Update Weights
+        change = np.outer(activations[i], error)
+        weights[f'W{i}'] += learning_rate * change
+
+        # Update output_errors for next iteration
+        output_errors = error
+
     return weights
 
-def create_nn(ninputs, nhiddenneurons, noutputs):
-    wsize = ((ninputs + 1) * nhiddenneurons) + (nhiddenneurons * noutputs)  # +1 for bias
-    np.random.seed(42)  # for reproducibility
-    weights = np.random.rand(1, wsize) - 0.5
-    return {'w': weights, 'ni': ninputs, 'nh': nhiddenneurons, 'no': noutputs}
+def create_nn(layer_sizes):
+    weights = {}
+    for i in range(len(layer_sizes) - 1):
+        weights[f'W{i}'] = np.random.rand(layer_sizes[i] + 1, layer_sizes[i + 1]) - 0.5  # +1 for bias
+    return weights
 
-def eval_nn(inputs, individual, ninputs, nhiddenneurons, noutputs):
-    ninputs += 1
-    
-    wi = individual[:ninputs * nhiddenneurons].reshape((ninputs, nhiddenneurons))
-    wo = individual[ninputs * nhiddenneurons:].reshape((nhiddenneurons, noutputs))
-    
-    ai = np.append(inputs, 1)
-    
-    sum_wi = np.dot(ai, wi)
-    ah = np.tanh(sum_wi)
-    
-    sum_wo = np.dot(ah, wo)
-    output = np.tanh(sum_wo)
-    
-    return output
+def eval_nn(inputs, weights):
+    activation = np.append(inputs, 1)  # Adding bias term
+    for i in range(len(weights)):
+        net_input = np.dot(activation, weights[f'W{i}'])
+        activation = np.tanh(net_input)
+        activation = np.append(activation, 1)  # Adding bias term for next layer
+    return activation[:-1]  # Remove the last bias term added
 
-def learning_ml_pk_out(input_data, nhiddenneurons, noutputs, error_limit, iterations_limit, output_flag):
-    A = input_data.copy()
+def learning_ml_pk_out(input_data, layer_sizes, error_limit, iterations_limit, output_flag):
+    N, nfeatures = input_data.shape
+    ninputs = nfeatures - layer_sizes[-1]  # Number of input features minus number of output neurons
     
-    N, ninputs = A.shape
-    ninputs -= noutputs
+    inputs = input_data[:, :ninputs]
+    targets = input_data[:, ninputs:]
+
+    weights = create_nn(layer_sizes)
     
-    actual_outputs = np.zeros((noutputs, N))
-    
-    inputs = A[:, :ninputs]
-    targets = A[:, ninputs:]
-    
-    wsize = ((ninputs + 1) * nhiddenneurons) + (nhiddenneurons * noutputs)
-    
-    net = create_nn(ninputs, nhiddenneurons, noutputs)
-    netw = net['w'].flatten()
-    
-    RMSE = 10
+    RMSE = float('inf')
     iteration = 0
     
-    while iteration <= iterations_limit and RMSE > error_limit:
+    while iteration < iterations_limit and RMSE > error_limit:
         iteration += 1
-        
+        total_error = 0
+
         for j in range(N):
-            output = eval_nn(inputs[j, :], netw, ninputs, nhiddenneurons, noutputs)
-            actual_outputs[:, j] = output
+            output = eval_nn(inputs[j, :], weights)
+            weights = backprop(weights, inputs[j, :], targets[j, :], layer_sizes)
+            total_error += np.sum((output - targets[j, :]) ** 2)
         
-        error = np.sum((actual_outputs - targets.T) ** 2) / noutputs
-        error1 = np.sum(error) / N
-        RMSE = np.sqrt(error1)
-        
+        RMSE = np.sqrt(total_error / (N * layer_sizes[-1]))
+
         if iteration % output_flag == 0:
-            print(f'Iteration {iteration}  Error {RMSE}')
-        
-        if RMSE <= error_limit:
-            break
-        
-        for j in range(N):
-            netw = backprop(netw, inputs[j, :], targets[j, :], ninputs, nhiddenneurons, noutputs)
-    
-    print(f'Iterations = {iteration}')
-    
-    for j in range(N):
-        output = eval_nn(inputs[j, :], netw, ninputs, nhiddenneurons, noutputs)
-        actual_outputs[:, j] = output
-    
-    return netw, iteration, RMSE
+            print(f'Iteration {iteration}, Error {RMSE}')
+
+    return weights, iteration, RMSE
 
 def reassignment(input_data, noutputs):
     N, m = input_data.shape
@@ -120,42 +91,26 @@ def reassignment(input_data, noutputs):
     
     return input_data
 
-def testing_ml_pk_out(input_data, nhiddenneurons, noutputs, netw):
-    noutputs = 3
-    N, ninputs = input_data.shape
-    
-    targets = input_data[:, -3:]
-    
-    for i in range(N):
-        if np.array_equal(targets[i, :], [1, -1, -1]):
-            targets[i, :] = [1, 0, 0]
-        elif np.array_equal(targets[i, :], [-1, 1, -1]):
-            targets[i, :] = [0, 1, 0]
-        elif np.array_equal(targets[i, :], [-1, -1, 1]):
-            targets[i, :] = [0, 0, 1]
-    
-    targets = np.argmax(targets, axis=1)  # Convert one-hot encoding to class labels
-    ninputs -= 3
-    
+def testing_ml_pk_out(input_data, weights, layer_sizes):
+    ninputs = layer_sizes[0]
+    N = input_data.shape[0]
+
     inputs = input_data[:, :ninputs]
-    actual_outputs = np.zeros(N)
-    
+    targets = input_data[:, ninputs:]
+
+    correct_predictions = 0
+
     for j in range(N):
-        output = eval_nn(inputs[j, :], netw, ninputs, nhiddenneurons, noutputs)
-        output_distance = np.abs(output - 1)
-        ind_output_neuron = np.argmin(output_distance)
-        actual_outputs[j] = ind_output_neuron
+        output = eval_nn(inputs[j, :], weights)
+        predicted_class = np.argmax(output)
+        actual_class = np.argmax(targets[j, :])
+        if predicted_class == actual_class:
+            correct_predictions += 1
+
+    classification_rate = (correct_predictions / N) * 100
+    print(f'Classification Rate = {classification_rate}%')
     
-    error = np.sum((actual_outputs - targets) ** 2) / N
-    RMSE = np.sqrt(error)
-    
-    results = (actual_outputs == targets)
-    num_of_correct_outputs = np.sum(results)
-    classif_rate = (num_of_correct_outputs / N) * 100
-    
-    print(f'Classification Rate = {classif_rate}')
-    
-    return classif_rate
+    return classification_rate
 
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -186,10 +141,10 @@ def generate_iris_dataset():
 import scipy.io
 
 # Load data from MLP_1 to MLP_4
-mlp_1 = scipy.io.loadmat('C://Users//Joel//Desktop//MLPSimulator//MLP_1.mat')['MLP_1']
-mlp_2 = scipy.io.loadmat('C://Users//Joel//Desktop//MLPSimulator//MLP_2.mat')['MLP_2']
-mlp_3 = scipy.io.loadmat('C://Users//Joel//Desktop//MLPSimulator//MLP_3.mat')['MLP_3']
-mlp_4 = scipy.io.loadmat('C://Users//Joel//Desktop//MLPSimulator//MLP_4.mat')['MLP_4']
+mlp_1 = scipy.io.loadmat('MLP_1.mat')['MLP_1']
+mlp_2 = scipy.io.loadmat('MLP_2.mat')['MLP_2']
+mlp_3 = scipy.io.loadmat('MLP_3.mat')['MLP_3']
+mlp_4 = scipy.io.loadmat('MLP_4.mat')['MLP_4']
 
 # Combine data from MLP_1 to MLP_4 into one learning set
 learning_set = np.concatenate((mlp_1, mlp_2, mlp_3, mlp_4), axis=0)
@@ -200,7 +155,7 @@ print(learning_set.shape)
 scipy.io.savemat('learning_set.mat', {'learning_set': learning_set})
 
 # Load data from MLP_5 as the test set
-test_set = scipy.io.loadmat('C://Users//Joel//Desktop//MLPSimulator//MLP_5.mat')['MLP_5']
+test_set = scipy.io.loadmat('MLP_5.mat')['MLP_5']
 
 
 # Save the test set to a new MATLAB file
@@ -211,15 +166,14 @@ scipy.io.savemat('test_set.mat', {'test_set': test_set})
 # Then, call the functions accordingly
 
 # For example:
-noutputs = 3
-nhiddenneurons = 4
 error_limit = 0.1
 iterations_limit = 100
 output_flag = 10
 
+layer_sizes = [4, 2, 6, 100, 3]
+
 #netw, iteration, RMSE = learning_ml_pk_out(learning_set, nhiddenneurons, noutputs, error_limit, iterations_limit, output_flag)
 #classif_rate = testing_ml_pk_out(test_set, nhiddenneurons, noutputs, netw)
-
 
 # Generating the Iris dataset
 learning_data, test_data = generate_iris_dataset()
@@ -227,5 +181,5 @@ learning_data, test_data = generate_iris_dataset()
 # Displaying a small part of the generated dataset for verification
 learning_data[:5], test_data[:5]
 
-netw, iteration, RMSE = learning_ml_pk_out(learning_data, nhiddenneurons, noutputs, error_limit, iterations_limit, output_flag)
-classif_rate = testing_ml_pk_out(test_set, nhiddenneurons, noutputs, netw)
+weights, iteration, RMSE = learning_ml_pk_out(learning_data, layer_sizes, error_limit, iterations_limit, output_flag)
+classif_rate = testing_ml_pk_out(test_set, weights, layer_sizes)

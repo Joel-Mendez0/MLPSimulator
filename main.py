2cb6bd69ce16fb24e85ed53ca256abd33d50c20c
naderabdullah <112974974+naderabdullah@users.noreply.github.com>
2023-12-13 09:26:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,88 +1,88 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def backprop(weights, inputs, targets, layer_sizes, learning_rate=0.01):
+def backpropagation(network_weights, input_values, target_values, neural_layers, rate_learning=0.01):
     # Forward Pass
-    activations = [np.append(inputs, 1)]  # Include bias in inputs
-    for i in range(len(layer_sizes) - 1):
-        net_input = np.dot(activations[-1], weights[f'W{i}'])
-        activation = np.tanh(net_input)
-        activation = np.append(activation, 1) if i < len(layer_sizes) - 2 else activation
-        activations.append(activation)
+    layer_activations = [np.append(input_values, 1)]  # Include bias in inputs
+    for layer_index in range(len(neural_layers) - 1):
+        layer_input = np.dot(layer_activations[-1], network_weights[f'Layer{layer_index}'])
+        layer_output = np.tanh(layer_input)
+        layer_output = np.append(layer_output, 1) if layer_index < len(neural_layers) - 2 else layer_output
+        layer_activations.append(layer_output)
 
     # Compute Output Error
-    output_errors = (1.0 - activations[-1] ** 2) * (targets - activations[-1])
+    output_errors = (1.0 - layer_activations[-1] ** 2) * (target_values - layer_activations[-1])
 
     # Backward Pass
-    for i in reversed(range(len(layer_sizes) - 1)):
-        if i != len(layer_sizes) - 2:  # Not the output layer
-            # Calculate error for hidden layers
-            error = (1.0 - activations[i + 1][:-1] ** 2) * np.dot(output_errors, weights[f'W{i+1}'].T)[:-1]
+    for layer_index in reversed(range(len(neural_layers) - 1)):
+        if layer_index != len(neural_layers) - 2:  # Not the output layer
+            layer_error = (1.0 - layer_activations[layer_index + 1][:-1] ** 2) * np.dot(output_errors, network_weights[f'Layer{layer_index+1}'].T)[:-1]
         else:
-            # Error for the output layer
-            error = output_errors
+            layer_error = output_errors
 
         # Compute Gradient and Update Weights
-        change = np.outer(activations[i], error)
-        weights[f'W{i}'] += learning_rate * change
+        weight_change = np.outer(layer_activations[layer_index], layer_error)
+        network_weights[f'Layer{layer_index}'] += rate_learning * weight_change
 
         # Update output_errors for next iteration
-        output_errors = error
+        output_errors = layer_error
 
-    return weights
+    return network_weights
 
-def create_nn(layer_sizes):
-    weights = {}
-    for i in range(len(layer_sizes) - 1):
-        weights[f'W{i}'] = np.random.rand(layer_sizes[i] + 1, layer_sizes[i + 1]) - 0.5  # +1 for bias
-    return weights
+def initialize_nn(neural_layers):
+    network_weights = {}
+    for layer_index in range(len(neural_layers) - 1):
+        network_weights[f'Layer{layer_index}'] = np.random.rand(neural_layers[layer_index] + 1, neural_layers[layer_index + 1]) - 0.5  # +1 for bias
+    return network_weights
 
-def eval_nn(inputs, weights):
-    activation = np.append(inputs, 1)  # Adding bias term
-    for i in range(len(weights)):
-        net_input = np.dot(activation, weights[f'W{i}'])
-        activation = np.tanh(net_input)
-        activation = np.append(activation, 1)  # Adding bias term for next layer
-    return activation[:-1]  # Remove the last bias term added
+def evaluate_nn(input_values, network_weights):
+    layer_output = np.append(input_values, 1)  # Adding bias term
+    for layer_index in range(len(network_weights)):
+        layer_input = np.dot(layer_output, network_weights[f'Layer{layer_index}'])
+        layer_output = np.tanh(layer_input)
+        layer_output = np.append(layer_output, 1)  # Adding bias term for next layer
+    return layer_output[:-1]  # Remove the last bias term added
 
-def learning_ml_pk_out(input_data, layer_sizes, error_limit, iterations_limit, output_flag):
-    N, nfeatures = input_data.shape
-    ninputs = nfeatures - layer_sizes[-1]  # Number of input features minus number of output neurons
+def train_nn(training_data, neural_layers, limit_error, limit_iterations, flag_output):
+    num_samples, num_features = training_data.shape
+    num_inputs = num_features - neural_layers[-1]  # Number of input features minus number of output neurons
 
-    inputs = input_data[:, :ninputs]
-    targets = input_data[:, ninputs:]
+    inputs = training_data[:, :num_inputs]
+    targets = training_data[:, num_inputs:]
 
-    weights = create_nn(layer_sizes)
+    network_weights = initialize_nn(neural_layers)
 
     RMSE = float('inf')
-    iteration = 0
-    errors = []  # Store errors for plotting
+    current_iteration = 0
+    iteration_errors = []  # Store errors for plotting
 
-    while iteration < iterations_limit and RMSE > error_limit:
-        iteration += 1
+    while current_iteration < limit_iterations and RMSE > limit_error:
+        current_iteration += 1
         total_error = 0
 
-        for j in range(N):
-            output = eval_nn(inputs[j, :], weights)
-            weights = backprop(weights, inputs[j, :], targets[j, :], layer_sizes)
-            total_error += np.sum((output - targets[j, :]) ** 2)
+        for sample_index in range(num_samples):
+            predicted_output = evaluate_nn(inputs[sample_index, :], network_weights)
+            network_weights = backpropagation(network_weights, inputs[sample_index, :], targets[sample_index, :], neural_layers)
+            total_error += np.sum((predicted_output - targets[sample_index, :]) ** 2)
 
-        RMSE = np.sqrt(total_error / (N * layer_sizes[-1]))
-        errors.append(RMSE)  # Store the error for plotting
+        RMSE = np.sqrt(total_error / (num_samples * neural_layers[-1]))
+        iteration_errors.append(RMSE)  # Store the error for plotting
 
-        if iteration % output_flag == 0:
-            print(f'Iteration {iteration}, Error {RMSE}')
+        if current_iteration % flag_output == 0:
+            print(f'Iteration {current_iteration}, Error {RMSE}')
 
     # Plot learning curve
     plt.figure(figsize=(8, 6))
-    plt.plot(range(1, iteration + 1), errors, marker='o')
+    plt.plot(range(1, current_iteration + 1), iteration_errors, marker='o')
     plt.xlabel('Iteration')
     plt.ylabel('Root Mean Squared Error')
     plt.title('Learning Curve')
     plt.grid(True)
     plt.show()
 
-    return weights, iteration, RMSE
+    return network_weights, current_iteration, RMSE
+
+# Remaining code for loading data and example usage remains unchanged
 
 def reassignment(input_data, noutputs):
     N, m = input_data.shape
@@ -95,39 +95,39 @@ def reassignment(input_data, noutputs):
     
     return input_data
 
-def testing_ml_pk_out(input_data, weights, layer_sizes):
-    ninputs = layer_sizes[0]
-    N = input_data.shape[0]
+def evaluate_model_performance(test_dataset, network_weights, neural_layers):
+    num_input_neurons = neural_layers[0]
+    num_samples = test_dataset.shape[0]
 
-    inputs = input_data[:, :ninputs]
-    targets = input_data[:, ninputs:]
+    input_features = test_dataset[:, :num_input_neurons]
+    target_values = test_dataset[:, num_input_neurons:]
 
-    correct_predictions = 0
-    actual_outputs = []
+    count_correct_predictions = 0
+    predicted_classes = []
 
-    for j in range(N):
-        output = eval_nn(inputs[j, :], weights)
-        predicted_class = np.argmax(output)
-        actual_class = np.argmax(targets[j, :])
-        actual_outputs.append(predicted_class)
+    for sample_index in range(num_samples):
+        model_output = evaluate_nn(input_features[sample_index, :], network_weights)
+        predicted_label = np.argmax(model_output)
+        actual_label = np.argmax(target_values[sample_index, :])
+        predicted_classes.append(predicted_label)
 
-        if predicted_class == actual_class:
-            correct_predictions += 1
+        if predicted_label == actual_label:
+            count_correct_predictions += 1
 
-    classification_rate = (correct_predictions / N) * 100
-    print(f'Classification Rate = {classification_rate}%')
+    accuracy_classification = (count_correct_predictions / num_samples) * 100
+    print(f'Classification Accuracy = {accuracy_classification}%')
 
     # Plotting
     plt.figure(figsize=(8, 6))
-    plt.plot(targets.argmax(axis=1), 'or', label='True Classes')
-    plt.plot(actual_outputs, '*b', label='Predicted Classes')
+    plt.plot(target_values.argmax(axis=1), 'or', label='True Classes')
+    plt.plot(predicted_classes, '*b', label='Predicted Classes')
     plt.xlabel('Sample Index')
     plt.ylabel('Class')
     plt.legend()
     plt.title('True vs Predicted Classes')
     plt.show()
 
-    return classification_rate
+    return accuracy_classification
 
 import scipy.io
 
